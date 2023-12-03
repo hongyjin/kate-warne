dd0af5fe9b591f8a8d7788a840746fe6b9164b57
hyen <hi020514@gmail.com>
2023-12-03 21:01:05 +0900

    fix: 경로수정2

--- kate-warne/src/katewarne/Papers.java
+++ kate-warne/src/katewarne/Papers.java
@@ -4,7 +4,7 @@ import javax.swing.ImageIcon;
 import java.awt.Image;
 
 public class Papers extends Clue {
-	//private Image papers = new ImageIcon("C:\\Users\\user\\Downloads\\자바 일러스트 모음\\범행 기록 문서.png").getImage();
+
 	private Image papers = new ImageIcon("./assets/images/범행 기록 문서.png").getImage();
 	public Papers(int pos_x, int pos_y) {
 		super(pos_x, pos_y);
